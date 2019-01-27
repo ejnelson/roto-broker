8c447cf3378bb831e0ccf645fa30482f9068f897
enelso26 <erik.nelson@optum.com>
2019-01-27 17:04:23 -0600

    test try catch and calling callback

--- src/lambda/testNewStuff.js
+++ src/lambda/testNewStuff.js
@@ -3,8 +3,6 @@
 import { google } from "googleapis";
 import fetch from "node-fetch";
 
-require("dotenv").config();
-
 const serviceAccount = {
   type: process.env.FB_TYPE,
   project_id: process.env.FB_PROJECT_ID,
@@ -17,14 +15,11 @@ const serviceAccount = {
   auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER,
   client_x509_cert_url: process.env.FB_CLIENT_CERT
 };
+
 const scopes = [
   "https://www.googleapis.com/auth/userinfo.email",
   "https://www.googleapis.com/auth/firebase.database"
 ];
-const fixedKey = serviceAccount.private_key.replace(
-  new RegExp("\\\\n", "g"),
-  "\n"
-);
 
 // Authenticate a JWT client with the service account.
 const jwtClient = new google.auth.JWT(
@@ -33,54 +28,32 @@ const jwtClient = new google.auth.JWT(
   serviceAccount.private_key.replace(new RegExp("\\\\n", "g"), "\n"),
   scopes
 );
-const options = {
-  headers: { "Ocp-Apim-Subscription-Key": process.env.FANTASY_API_KEY }
-};
 function checkStatus(res) {
+  console.log(`status${res.status}`);
   if (res.ok) {
     // res.status >= 200 && res.status < 300
     return res;
   }
-  throw new Error("bad fetch");
+  throw new Error(res.statusText);
 }
-export async function handler(event, context, callback) {
-  console.log(`10.............................................`);
-  console.log(`email   ${serviceAccount.client_email}`);
-  console.log(`private key  ${serviceAccount.private_key}`);
-  console.log(`fixed key ${fixedKey}`);
-
+export function handler(event, context, callback) {
   try {
-    //   const response = await fetch(
-    //     "https://api.fantasydata.net/v3/nfl/stats/JSON/FantasyPlayers",
-    //     options
-    //   )
-    //     .then(res => res.json())
-    //     .then(json => {
-    console.log("got it");
     // Use the JWT client to generate an access token.
     jwtClient.authorize(async (error, tokens) => {
-      console.log("Im gunna try to write to firebase now please");
       if (error) {
         console.log("Error making request to generate access token:", error);
-        callback({
-          statusCode: 400,
-          body: JSON.stringify({
-            message: "error1"
-          })
-        });
       } else if (tokens.access_token === null) {
         console.log(
           "Provided service account does not have permission to generate access tokens"
         );
       } else {
         const accessToken = tokens.access_token;
-        console.log(`here is the TOKENNNNNNNNN${accessToken}`);
         const write = await fetch(
           `https://roto-broker-625b9.firebaseio.com/nflData.json?access_token=${accessToken}`,
           {
             body: JSON.stringify({
               // json
-              test: "ok"
+              test: "ok123"
             }),
             headers: {
               "Content-Type": "application/x-www-form-urlencoded"
@@ -89,35 +62,32 @@ export async function handler(event, context, callback) {
           }
         )
           .then(checkStatus)
-          .catch(err => {
-            console.error(err);
-            callback({
-              statusCode: 400,
-              body: JSON.stringify({
-                message: "error2"
-              })
-            });
-          })
           .then(res => res.json())
           .then(json => {
             console.log(json);
-            callback({
+            // return {
+            //   statusCode: 200,
+            //   body: JSON.stringify({
+            //     posted: json
+            //   })
+            // };
+            return callback({
               statusCode: 200,
               body: JSON.stringify({
-                msg: "wedidit!"
+                posted: json
               })
             });
           });
+        write();
       }
     });
-    // });
-  } catch (err) {
-    console.log(err); // output to netlify function log
+  } catch {
+    console.log("ERROR");
 
     callback({
       statusCode: 400,
       body: JSON.stringify({
-        message: "error3"
+        message: "ERROR SOMEWHER"
       })
     });
   }
