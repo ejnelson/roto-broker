58b25ef05685c1955a6a2ad7920ca7b5d8eafb41
enelso26 <erik.nelson@optum.com>
2019-10-15 10:55:04 -0500

    started using gatsby firebase auth starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,19 @@
+require('dotenv').config({
+  path: `.env/.env.${process.env.NODE_ENV}`
+});
+
 const path = require(`path`);
 
 module.exports = {
   siteMetadata: {
-    title: "Roto-Broker",
-    description:
-      "Making it easy to get fantasy football trades done with your league mates!",
-    url: "https://www.roto-broker.com", // No trailing slash allowed!
-    author: "@ejnelson"
+    title: 'Roto-Broker',
+    description: 'Making it easy to get fantasy football trades done with your league mates!',
+    url: 'https://www.roto-broker.com', // No trailing slash allowed!
+    author: '@ejnelson'
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -30,22 +33,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Roto-Broker",
-        short_name: "Roto-Bro",
-        start_url: "/",
-        background_color: "#8c52ff",
-        theme_color: "#8c52ff",
-        display: "minimal-ui",
-        icon: "src/images/rotobrokericon.png" // This path is relative to the root of the site.
+        name: 'Roto-Broker',
+        short_name: 'Roto-Bro',
+        start_url: '/',
+        background_color: '#8c52ff',
+        theme_color: '#8c52ff',
+        display: 'minimal-ui',
+        icon: 'src/images/rotobrokericon.png' // This path is relative to the root of the site.
       }
     },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    },
-    "gatsby-plugin-layout",
-    "gatsby-plugin-offline"
+    'gatsby-plugin-offline'
   ]
 };
